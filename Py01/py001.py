c4dc7ca3cfe8d94f1fdbe6377a21fcb5d70ef2b7
AlicePicky <alicepicky@robo-deep.com>
2026-07-01 10:00:17 +0800

    基础语法的了解

---
+++ Py01/py001.py
@@ -0,0 +1,34 @@
+from lib2to3.btm_matcher import type_repr
+
+print('hello world')
+print(False + 10)
+print('sis')
+print("sister")
+print("""d
+哈喽小世界
+""")
+
+var1 = 3.14159
+print("%3f" % var1)
+
+var2 = 34
+print("%010d" % var2)
+
+
+print("胜利的概率是98%%" % ())
+
+name = "小李子"
+age = 20
+print(f"我的名字是{name},年龄是{age}")
+
+
+n1 = 99
+print(n1)
+n2 = 66
+n1 += n2
+print(n1)
+
+pwd = input("请输入你的密码：")
+print(type(pwd))
+isinstance(pwd,int)
+print(type(pwd))
