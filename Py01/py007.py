223cf616e1199075165bbc3d8e2b540037f1b3da
AlicePicky <alicepicky@robo-deep.com>
2026-07-08 16:05:29 +0800

    面向对象的一些学习，从定义初始化和语言特性上作学习

--- Py01/py007.py
+++ Py01/py007.py
@@ -50,18 +50,59 @@
 # study("bingbing")
 #
 # # 多个参数的装饰器函数
-from keyword import kwlist
-
 # @outer#语法糖侵入
-def send(*args,**kwargs):
-    print(args)
-    print(kwargs)
+# def send(*args,**kwargs):
+#     print(args)
+#     print(kwargs)
+#
+# def outer(fn):
+#     def inner(*args,**kwargs):
+#         print("登陆。。。")
+#         fn(*args,**kwargs)
+#     return inner
+#
+# ot = outer(send)
+# ot("123",name = "bingbing",age = 19)
+# class Washer:
+#
+#     def __init__(self,width,height):
+#         self.Width = width
+#         self.Height = height
+#     def wash(self):
+#         print("washing...")
+#
+# washer1 = Washer(600,300)
+# print(washer1.Width)
+# print(washer1.Height)
+# washer1.wash()
+#
+# washer1.time = 2000
+# print(washer1.time)
+# print(washer1.__dict__)
+# print(Washer.__dict__)
+#
 
-def outer(fn):
-    def inner(*args,**kwargs):
-        print("登陆。。。")
-        fn(*args,**kwargs)
-    return inner
+class Person:
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
 
-ot = outer(send)
-ot("123",name = "bingbing",age = 19)
+    def study(self):
+        print(self.name + " is studying...")
+
+    def eat(self):
+        print(self.name +" is eating...")
+
+p1 = Person("bingbing",18,"female")
+p2 = Person("xiaoming",22,"male")
+print(p1.name,p1.age,p1.sex)
+print(p2.name,p2.age,p2.sex)
+print()
+p1.study()
+p1.eat()
+print()
+p2.study()
+p2.eat()
+
+
