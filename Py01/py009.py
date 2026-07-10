5137943d5e1544a17b3a213327b9d58914210aab
AlicePicky <alicepicky@robo-deep.com>
2026-07-10 11:44:09 +0800

    关于python单例模式的几种创建方式

--- Py01/py009.py
+++ Py01/py009.py
@@ -14,15 +14,51 @@
 # p1 = Person()
 # print("p1:",p1)
 # p1.introduce()
+#
+#
+# class Person:
+#     def __new__(cls, *args, **kwargs):
+#         print("重写了new方法")
+#         return super().__new__(cls)
+#
+#     def __init__(self,name = None):
+#         print("名字是：",name)
+#
+# p1 = Person("xiaomi")
+# p2 = Person("xiaohong")
 
-
-class Person:
+# 单例模式
+#通过重写__new__ 方法实现单例模式
+class SingleTon(object):
+    _instance = None
     def __new__(cls, *args, **kwargs):
-        print("重写了new方法")
-        return super().__new__(cls)
+        print("创建单例")
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    def __init__(self):
+        print("init()方法")
 
-    def __init__(self,name = None):
-        print("名字是：",name)
+s1 = SingleTon()
+print("s1",s1)
+s2 = SingleTon()
+print("s2",s2)
 
-p1 = Person("xiaomi")
-p2 = Person("xiaohong")
+
+# 通过装饰器实现
+def SingleTon1(cls):
+    instance = {}
+    def wrapper(*args,**kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args,**kwargs)
+        return instance[cls]
+    return wrapper
+
+@SingleTon1
+class User:
+    pass
+a = User()
+b = User()
+print("a",a)
+print("b",b)
+
