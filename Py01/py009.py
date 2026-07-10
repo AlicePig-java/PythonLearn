dbd31dbf091b011722ad2c38abf03f6010064e66
AlicePicky <alicepicky@robo-deep.com>
2026-07-10 11:01:15 +0800

    关于new方法和init方法的学习

---
+++ Py01/py009.py
@@ -0,0 +1,28 @@
+#对于__new__ 方法和__init__方法
+# class Person:
+#     name = "xiaoming"#类属性
+#
+#     def __init__(self):
+#         self.age = 19#实例属性
+#
+#     def __new__(cls, *args, **kwargs):
+#         print("重写了object中的new方法")#重写new方法必须要手动返回object中的new方法引用，不然会导致对象初始化引用没有获取到
+#         return  super().__new__(cls)
+#
+#     def introduce(self):
+#         print(f"{Person.name}:{self.age}")#实例方法
+# p1 = Person()
+# print("p1:",p1)
+# p1.introduce()
+
+
+class Person:
+    def __new__(cls, *args, **kwargs):
+        print("重写了new方法")
+        return super().__new__(cls)
+
+    def __init__(self,name = None):
+        print("名字是：",name)
+
+p1 = Person("xiaomi")
+p2 = Person("xiaohong")
